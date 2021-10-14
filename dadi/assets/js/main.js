07e628d6a82257315fcfd4757b497e64d330d165
Nicola-Perera <nicola.perera96@mail.com>
2021-10-14 17:39:06 +0200

    add 2 random numbers from 1 to 1st number goes to user, 2nd number goes to computer

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -1,1 +1,9 @@
 // programma per far giocare a dadi utente vs computer
+
+alert('IL GIOCO DEI DADI: premi invio lanciare un dado a 6 facce. Se il tuo risultato supera quello del banco HAI VINTO !!');
+let userDieResult = Math.floor(Math.random() * 6 + 1);
+console.log(userDieResult);
+
+let computerDieResult = Math.floor(Math.random() * 6 + 1);
+console.log(computerDieResult);
+
