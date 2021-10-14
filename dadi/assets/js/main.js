8a3f0f322dadefb716b1846f8d16cc652d35a382
Nicola-Perera <nicola.perera96@mail.com>
2021-10-14 18:15:41 +0200

    add dice roll results and who won to html

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -5,29 +5,27 @@ alert('IL GIOCO DEI DADI: premi invio lanciare un dado a 6 facce. Se il tuo risu
 
 // user roll
 let userDieResult = Math.floor(Math.random() * 6 + 1);
-console.log(userDieResult);
+const userNumber = document.getElementById('user_roll');
+userNumber.innerHTML = 'hai fatto ' + userDieResult;
 
 // computer roll
 let computerDieResult = Math.floor(Math.random() * 6 + 1);
-console.log(computerDieResult);
-
-
-const userNumber = document.getElementById('user_roll');
 const computerNumber = document.getElementById('computer_roll');
-const rollResult = document.getElementById('dice_result');
+computerNumber.innerHTML = 'il banco ha fatto ' + computerDieResult;
 
+const rollResult = document.getElementById('dice_result');
 
 // win-lose-draw conditions
 if (userDieResult > computerDieResult) {
     const win = `<h1>HAI VINTO :D</h1>`;
-    rollResult.innerHTML += win;
+    rollResult.innerHTML = win;
 }
 else if (userDieResult < computerDieResult) {
     const lose = `<h1>hai perso :(</h1>`;
-    rollResult.innerHTML += lose;
+    rollResult.innerHTML = lose;
 }
 else if (userDieResult === computerDieResult) {
     const draw = `<h1>Pareggio! riprova</h1>`;
-    rollResult.innerHTML += draw;
+    rollResult.innerHTML = draw;
 }
 
