ea1b88319db4cc134af2871d88d42e7300c6d1a4
Nicola-Perera <nicola.perera96@mail.com>
2021-10-14 15:40:42 +0200

    add array with members emails

---
+++ mail/assets/js/main.js
@@ -0,0 +1,19 @@
+// programma per verificare la presenza nella lista membri della mail inserita dall'utente
+
+const members = [
+    'corda.valerio05@gmail.com',
+    'lcostin40@gmail.com',
+    '4lek.90@gmail.com',
+    'diegodenuccio95@gmail.com',
+    'samudichio@gmail.com',
+    'federico.frasca796@gmail.com',
+    'fregonesematteo2@gmail.com',
+    'doinaganceriuc@gmail.com',
+    'nicola.lilla@gmail.com',
+    'lorenzo.lumilio@gmail.com',
+    'lucamgns33@gmail.com',
+    'alessia.misciagna98@gmail.com',
+    'nicola.perera96@gmail.com'
+];
+
+console.log(members);
